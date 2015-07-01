cc18ca6b8a06290f61676d836657485ad91c8391
Jonas Heinrich <j.heinrich@finn.de>
2015-07-01 16:54:35 +0200

    Creates own class for i18n provider

---
+++ lib/open_project/translations/models/i18n_provider.rb
@@ -0,0 +1,57 @@
+require 'yaml'
+
+require_relative './locales_updater_configuration.rb'
+
+class I18nProvider
+  def initialize(project_id, api_key, crowdin_directory)
+    @project_id = project_id
+    @api_key = api_key
+    @crowdin_directory = crowdin_directory
+  end
+
+  def create_handle
+    Crowdin::API.new project_id: @project_id, api_key: @api_key
+  end
+
+  def configuration
+    LocalesUpdaterConfiguration.configuration
+  end
+
+  def upload_english(translation_file, path_to_translation, title)
+    crowdin = create_handle
+
+    # create crowdin directory just in case it doesn't exist.
+    dir = crowdin.project_info['files'].find {|f| f['name'] == @crowdin_directory && f['node_type'] == 'directory'}
+    unless dir
+      crowdin.add_directory(@crowdin_directory)
+    end
+
+    dir = crowdin.project_info['files'].find {|f| f['name'] == @crowdin_directory && f['node_type'] == 'directory'}
+
+    if dir['files'].find {|f| f['name'] == translation_file}
+      crowdin.update_file [{dest: "/#{@crowdin_directory}/#{translation_file}",
+                            source: path_to_translation.to_s,
+                            title: title,
+                            export_pattern: '%two_letters_code%.yml'}],
+                            type: 'yaml'
+    else
+      crowdin.add_file [{dest: "/#{@crowdin_directory}/#{translation_file}",
+                         source: path_to_translation.to_s,
+                         title: title,
+                         export_pattern: '%two_letters_code%.yml'}],
+                         type: 'yaml'
+    end
+  end
+
+  def request_build
+    # todo maybe this could run into a timeout?
+    crowdin = create_handle
+    crowdin.export_translations
+  end
+
+  def download_locales(output)
+    # todo what about errors? maybe a timeout?
+    crowdin = create_handle
+    crowdin.download_translation 'all', output: output
+  end
+end
