5a485e01d76d56d95a323229ad6b70b74c0f0614
Jonas Heinrich <j.heinrich@finn.de>
2015-07-01 16:54:54 +0200

    Adds a rather important todo

--- lib/open_project/translations/helpers/run_command.rb
+++ lib/open_project/translations/helpers/run_command.rb
@@ -1,5 +1,6 @@
 module RunCommand
   def run_command(command)
+    # todo we need the output from the command e.g. for the branch in GitRepository
     # todo we should check if the command succeeds.
     # especially if we use mixlib's shellout which suppresses the output
     system(command)
