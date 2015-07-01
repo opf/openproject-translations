f670828e2db3ea97993309224d351482ef8e9f3f
Jonas Heinrich <j.heinrich@finn.de>
2015-07-01 10:56:55 +0200

    Restructures tmp folders

--- lib/open_project/translations/helpers/tmp_directory.rb
+++ lib/open_project/translations/helpers/tmp_directory.rb
@@ -1,9 +1,24 @@
 module TmpDirectory
-  def create_tmp_directory(delete: false)
-    tmp_path = File.join(FileUtils.pwd, 'tmp')
-    if delete
-      FileUtils.rm_rf tmp_path
+  def within_tmp_directory(delete_if_exists: false, path: 'tmp', debug: false)
+    create_tmp_directory(delete_if_exists: delete_if_exists, path: path)
+
+    Dir.chdir path do
+      yield if block_given?
     end
+
+    remove_tmp_directory(path) unless debug
+  end
+
+  def create_tmp_directory(delete_if_exists: false, path: 'tmp')
+    if path == 'tmp'
+      tmp_path = File.join(FileUtils.pwd, path)
+    else
+      tmp_path = path
+    end
+    if delete_if_exists
+      remove_tmp_directory(tmp_path)
+    end
+
     FileUtils.mkdir_p tmp_path
     tmp_path
   end
