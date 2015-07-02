a4675d4b2db296060524c7cc5e4d56bcb8e3ddb4
Jonas Heinrich <j.heinrich@finn.de>
2015-07-02 11:16:57 +0200

    Do not raise an error when there is nothing to commit

--- lib/open_project/translations/helpers/run_command.rb
+++ lib/open_project/translations/helpers/run_command.rb
@@ -4,7 +4,8 @@ module RunCommand
   def run_command(command)
     shell = Mixlib::ShellOut.new(command)
     shell.run_command
-    raise "The following command returned an error: #{command}" if shell.error?
+    raise "The following command returned an error: #{command}" if shell.error? &&
+      !shell.stdout =~ /nothing to commit/
 
     shell.stdout.gsub(/\n$/, '')
   end
