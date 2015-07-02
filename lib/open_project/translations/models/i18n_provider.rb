0ad6068bd4663d48c10c538110dab79b032619e9
Jonas Heinrich <j.heinrich@finn.de>
2015-07-02 11:37:45 +0200

    Makes the crowdin handle an instance variable

--- lib/open_project/translations/models/i18n_provider.rb
+++ lib/open_project/translations/models/i18n_provider.rb
@@ -8,6 +8,7 @@ class I18nProvider
     @project_id = project_id
     @api_key = api_key
     @crowdin_directory = crowdin_directory
+    @crowdin = create_handle
   end
 
   def create_handle
@@ -19,8 +20,8 @@ class I18nProvider
   end
 
   def upload_english(translation_file, path_to_translation, title)
-    # create crowdin directory just in case it doesn't exist.
     begin
+      # create crowdin directory just in case it doesn't exist.
       add_directory_if_missing
 
       if file_exists_in_directory?(translation_file)
@@ -38,21 +39,18 @@ class I18nProvider
   end
 
   def add_directory_if_missing
-    crowdin = create_handle
-    unless crowdin.project_info['files'].find {|f| f['name'] == @crowdin_directory && f['node_type'] == 'directory'}
-      crowdin.add_directory(@crowdin_directory)
+    unless @crowdin.project_info['files'].find {|f| f['name'] == @crowdin_directory && f['node_type'] == 'directory'}
+      @crowdin.add_directory(@crowdin_directory)
     end
   end
 
   def file_exists_in_directory?(file)
-    crowdin = create_handle
-    directory = crowdin.project_info['files'].find {|f| f['name'] == @crowdin_directory && f['node_type'] == 'directory'}
+    directory = @crowdin.project_info['files'].find {|f| f['name'] == @crowdin_directory && f['node_type'] == 'directory'}
     directory['files'].find {|f| f['name'] == file}
   end
 
   def update_file(dest:, source:, title:)
-    crowdin = create_handle
-    crowdin.update_file [{dest: dest,
+    @crowdin.update_file [{dest: dest,
                           source: source,
                           title: title,
                           export_pattern: '%two_letters_code%.yml'}],
@@ -60,8 +58,7 @@ class I18nProvider
   end
 
   def add_file(dest:, source:, title:)
-    crowdin = create_handle
-    crowdin.add_file [{dest: dest,
+    @crowdin.add_file [{dest: dest,
                           source: source,
                           title: title,
                           export_pattern: '%two_letters_code%.yml'}],
@@ -71,8 +68,7 @@ class I18nProvider
   def request_build
     # todo maybe this could run into a timeout?
     begin
-      crowdin = create_handle
-      crowdin.export_translations
+      @crowdin.export_translations
     rescue Crowdin::API::Errors::Error => e
       puts "Error during update of #{@project_id}: #{e.message}"
     end
@@ -81,8 +77,7 @@ class I18nProvider
   def download_locales(output)
     # todo what about errors? maybe a timeout?
     begin
-      crowdin = create_handle
-      crowdin.download_translation 'all', output: output
+      @crowdin.download_translation 'all', output: output
     rescue Crowdin::API::Errors::Error => e
       puts "Error during update of #{@project_id}: #{e.message}"
     end
@@ -90,8 +85,7 @@ class I18nProvider
 
   def translation_status_high_enough?(code, percent)
     @translations_statuses ||= begin
-      crowdin = create_crowdin_handle
-      crowdin.translations_status
+      @crowdin.translations_status
     end
     translation_status = @translations_statuses.select do |translation|
       translation['code'] == code
